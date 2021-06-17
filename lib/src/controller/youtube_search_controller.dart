4d304ee8b23111131303ed7882b27292ddfdb90a
MG_dev <68521263+mugon-dev@users.noreply.github.com>
2021-06-18 03:07:16 +0900

    add if를 통한 검색 기록 중복 방지

--- lib/src/controller/youtube_search_controller.dart
+++ lib/src/controller/youtube_search_controller.dart
@@ -5,25 +5,21 @@ class YoutubeSearchController extends GetxController {
   String key = 'SearchKey';
   // RxList 초기화 또는 [""].obs 로 대체 가능
   RxList<String> history = RxList<String>.empty(growable: true);
-  late SharedPreferences profs;
-  // set을 통해 중복 처리
-  Set<String> originHistory = {};
+  late SharedPreferences _profs;
 
   @override
   void onInit() async {
     // local storage를 통해 관리
-    profs = await SharedPreferences.getInstance();
-    List<dynamic> initData = profs.get(key) as List;
-    originHistory.addAll(initData.map((_) => _.toString()).toList());
-    history(originHistory.toList());
+    _profs = await SharedPreferences.getInstance();
+    List<dynamic> initData = _profs.get(key) as List;
+    history(initData.map((_) => _.toString()).toList());
 
     super.onInit();
   }
 
   void search(String search) {
-    history.clear(); // history 초기화
-    originHistory.add(search);
-    history.addAll(originHistory.toList());
-    profs.setStringList(key, history.toList());
+    //add if 를 통해 넣을 값이 존재하지 않으면 저
+    history.addIf(!history.contains(search), search);
+    _profs.setStringList(key, history.toList());
   }
 }
