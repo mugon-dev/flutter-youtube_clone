33f4b1b201231f3998d1c91c7d0ad62ca387fc3e
MG_dev <68521263+mugon-dev@users.noreply.github.com>
2021-06-17 20:13:58 +0900

    create main app bar

---
+++ lib/src/components/custom_appbar.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          _logo(),
+          _actions(),
+        ],
+      ),
+    );
+  }
+
+  Widget _actions() {
+    return Row(
+      children: [
+        GestureDetector(
+          onTap: () {},
+          child: Container(
+            width: 23,
+            height: 23,
+            child: SvgPicture.asset("assets/svg/icons/bell.svg"),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: GestureDetector(
+            onTap: () {},
+            child: Container(
+              width: 23,
+              height: 23,
+              child: SvgPicture.asset("assets/svg/icons/search.svg"),
+            ),
+          ),
+        ),
+        CircleAvatar(
+          backgroundColor: Colors.grey.withOpacity(0.5),
+          backgroundImage: Image.network(
+                  "https://yt3.ggpht.com/ytc/AAUvwniU0ZOGv47lDdGSQ8H004fQgwOAJRlobuCvXwNl=s48-c-k-c0x00ffffff-no-rj")
+              .image,
+        )
+      ],
+    );
+  }
+
+  Widget _logo() {
+    return Container(
+      child: Image.asset(
+        'assets/images/logo.png',
+        width: 130,
+      ),
+    );
+  }
+}
--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -1,15 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_clone/src/components/custom_appbar.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text("HOME"),
+    return SafeArea(
+      child: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: CustomAppBar(),
+            // 밑으로 스크롤할땐 app바 사라졌다가 다시 위로 스크롤하면 app바 나타남
+            floating: true,
+            snap: true,
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 150,
+                    color: Colors.grey,
+                  ),
+                );
+              },
+              childCount: 10,
+            ),
+          )
+        ],
       ),
     );
+    ;
   }
 }
